7936a3f38f9f574b02a6a93b2151bbe8ed57bd27
djtoohey <djtoohey@gmail.com>
2020-10-05 15:20:20 +1030

    ghpages react add to package.json

---
+++ build/precache-manifest.9161719b2837493ffe54280387c191b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd71d0075c17208bfe05b10f8da59792",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "4f878b8709945f522175",
+    "url": "/reactPortfolio/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "af3900422e8624aad165",
+    "url": "/reactPortfolio/static/css/main.aec7cd57.chunk.css"
+  },
+  {
+    "revision": "4f878b8709945f522175",
+    "url": "/reactPortfolio/static/js/2.11784e65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.11784e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3900422e8624aad165",
+    "url": "/reactPortfolio/static/js/main.537ca208.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  }
+]);
